84d6eea846045c665875b2627fd68ce1135a9483
Irosh Perera <iroshlashan31@gmail.com>
2024-07-14 12:01:15 +0530

    comments

---
+++ 03_comments.dart
@@ -0,0 +1,17 @@
+void main() {
+  // 03 types of comments in dart
+  // 1. Single line comment
+  // Single line comment is used to comment a single line
+  // Single line comment starts with two forward slashes - //
+
+  // 2. Multi line comment
+  // Multi line comment is used to comment multiple lines
+  // Multi line comment starts with forward slash and asterisk - /*
+  // Multi line comment ends with asterisk and forward slash - */
+
+  // 3. Documentation comment
+  // Documentation comment is used to generate documentation for the code
+  // Documentation comment starts with forward slash and two asterisks - ///
+
+
+}
