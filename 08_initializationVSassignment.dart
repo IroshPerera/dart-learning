682eeb9c3f55cc3a9fef9282ec243be86a2906cf
Irosh Perera <iroshlashan31@gmail.com>
2024-07-14 13:22:24 +0530

    initialization vs assignment

---
+++ 08_initializationVSassignment.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  // Initialization - Assigning a value to a variable for the first time
+  int num1 = 10;
+  int num2 = 20;
+  print("$num1 $num2");
+  
+  // Assignment - Assigning a value to a variable which is already initialized
+  num1 = 30;
+  num2 = 40;
+  print("$num1 $num2");
+
+
+}
