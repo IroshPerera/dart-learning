81a543ee143dcf06a0e0dbb60d9bf3867b53d529
Irosh Perera <iroshlashan31@gmail.com>
2024-07-21 10:25:18 +0530

    ternary operator

---
+++ 15_ternaryOperator.dart
@@ -0,0 +1,12 @@
+// Ternary Operator
+// The ternary operator is a one-liner replacement for if-else statement. It has three parts:
+
+// 01. Condition: The condition to evaluate.
+// 02. Expression1: The expression to execute if the condition is true.
+// 03. Expression2: The expression to execute if the condition is false.
+
+// Syntax:
+// condition ? expression1 : expression2
+
+var age = 18;
+age >= 18 ? print("You are eligible to vote.") : print("You are not eligible to vote.");
