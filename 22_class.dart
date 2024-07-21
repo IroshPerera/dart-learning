0e6a18c0544ce2116786bdc6069c8e9278312774
Irosh Perera <iroshlashan31@gmail.com>
2024-07-21 10:55:18 +0530

    class

---
+++ 22_class.dart
@@ -0,0 +1,18 @@
+// class 
+// - class is a blueprint for creating objects
+
+class Student {
+  int? id;
+  String? name;
+  double? percentage;
+  
+
+  void display() {
+    print('ID: $id');
+    print('Name: $name');
+    print('Percentage: $percentage');
+  }
+}
+
+// This example creates a class named Student with three fields: id, name, and percentage.
+
