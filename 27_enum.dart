fe8662f153aea1447742dc199bb50932d9067e5b
Irosh Perera <iroshlashan31@gmail.com>
2024-07-21 10:55:53 +0530

    enum

---
+++ 27_enum.dart
@@ -0,0 +1,16 @@
+// class Enum / Enumerations
+// Enum is a way to create a group of constants that are related to each other.
+
+
+enum Colors{
+  red,
+  green,
+  blue
+}
+
+void main() {
+  print(Colors.values);
+  print(Colors.red);
+  print(Colors.green);
+  print(Colors.blue);
+}
